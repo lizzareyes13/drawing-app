f6aae6fb20e0869d1dfa5da3f7edf456977206e9
Lizza Reyes <Lizzareyes13@gmail.com>
2016-06-29 19:59:24 -0400

    drawing-app

---
+++ js/app.js
@@ -0,0 +1,17 @@
+//When clicking on the color picking list items
+  //deselect all li's that are selected
+  //select the one which was clicked
+
+//When "new color" is pressed
+  //hide or unhide the color select div
+
+//When color sliders changes
+//OR when the previous event fires
+  //update the new color span
+
+//When add color is pressed
+  //append the new color to the controls ul
+  //select the new color
+
+//on mouse events on the canvas
+  //draw lines
